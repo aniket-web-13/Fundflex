6134786b697ec77cc2387540797af21a90acd056
aniket-web-13 <aniketweb13@gmail.com>
2025-01-08 16:34:14 +0530

    recent update nav

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,70 +1,80 @@
 
-import {NavLinkBtn , NavBtn} from "../cardComponents/shared/buttons.jsx"
+import { NavLinkBtn, NavBtn } from "../cardComponents/shared/buttons.jsx"
 import { NavImg } from "../cardComponents/shared/imgSec.jsx";
 
 const Nav = () => {
-    const navLink =[
+    const navLink = [
         {
-            nav:"All Page",
-            link:"/",
+            nav: "All Page",
+            link: "/",
         },
         {
-            nav:"Pricing",
-            link:"/pricing-page"
+            nav: "Pricing",
+            link: "/pricing-page"
         },
         {
-            nav:"About",
-            link:""
+            nav: "About",
+            link: ""
         },
         {
-            nav:"Contact Us",
-            link:""
+            nav: "Contact Us",
+            link: ""
         },
     ]
- 
-    const hamBtn = () => {        
+
+    const hamBtn = () => {
+        /* this function use for Sub Nav link box show and hide */
         let subLink = document.getElementById("subLInk");
-        subLink.classList.toggle("hamShow")    
+        subLink.classList.toggle("hamShow");
+        
+        
+
+
+      
+      
+        
     }
-    
-return(
-   <nav>
-        <div className="nav-padding">
-            <div className="container">
-                <div className="nav-main-count">
-                    <div className="nav-left">
-                        <NavImg />
-                    </div>
-                    <div className="nav-mid">
-                        {navLink.map((value , i) => {
-                            return(
-                                <NavLinkBtn data={value.nav} link={value.link} index={i}/>
-                            )
-                        })}
-                    </div>
-                    <div className="nav-right">
-                        <NavBtn navTitle={"Get started"} arrow={""}/>
-                        <div className="ham-count" onClick={hamBtn}>
-                            <div className="ham-line-1"></div>
-                            <div className="ham-line-2"></div>
-                            <div className="ham-line-3"></div>
+
+    return (
+        <nav>
+            <div className="nav-padding">
+                <div className="container">
+                    <div className="nav-main-count">
+                        <div className="nav-left">
+                            <NavImg />
                         </div>
-                    </div>                   
-                 </div>                               
-             </div>
-        </div>
-        <div className="sub-link-count" id="subLInk">
-            <div className="sub-in-box">
-                {navLink.map((value , i) => {
-                     return(
-                         <NavLinkBtn data={value.nav} link={value.link} index={i}/>
-                     )
-                 })}
+                        <div className="nav-mid">
+                            {navLink.map((value, i) => {
+                                return (
+                                    <NavLinkBtn data={value.nav} link={value.link} index={i} />
+                                )
+                            })}
+                        </div>
+                        <div className="nav-right">
+                            <div className="navbar-btn">
+                                <NavBtn navTitle={"Get started"} arrow={""} />
+                            </div>
+                            <div className="ham-count" onClick={hamBtn}>
+                                <div id="hamLine1"></div>
+                                <div id="hamLine2"></div>
+                                <div id="hamLine3"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-           
-        </div>      
-   </nav>
-)
+            <div className="sub-link-count" id="subLInk">
+                <div className="sub-in-box">
+                    {navLink.map((value, i) => {
+                        return (
+                            <NavLinkBtn data={value.nav} link={value.link} index={i} />
+                        )
+                    })}
+                </div>
+
+            </div>
+        </nav>
+    )
 };
 
 export default Nav;
